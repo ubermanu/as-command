c11efa911e07edb4a59b72b6056ff56161e8ff83
ubermanu <manu@paca.happy-dev.fr>
2023-09-15 09:51:40 +0200

    chore: split code and tests

--- assembly/command.ts
+++ assembly/command.ts
@@ -1,4 +1,5 @@
-export type ParsedArgs = Map<string, string[]>
+import { parseArgs, ParsedArgs } from './parse-args'
+
 export type CommandHandler = (args: ParsedArgs) => void
 
 export class Command {
@@ -103,74 +104,4 @@ export class Option {
     public description: string,
     public defaultValue: string[] = []
   ) {}
-}
-
-/**
- * Construct a map of parsed arguments. Regroup every option into a map. For the
- * arguments, they are stored into the `_` key.
- *
- * The `booleans` argument is used to specify which options are boolean. If an
- * option is boolean, it will stop looking for a value after it.
- *
- * @example
- *   const parsedArgs = this.parseArgs(['-p', '8080', '-h', 'localhost', 'index.html']);
- *
- *   parsedArgs = Map {
- *   '-p' => [ '8080' ],
- *   '-h' => [ 'localhost' ],
- *   '_' => [ 'index.html' ]
- *   }
- */
-export function parseArgs(args: string[], booleans: string[] = []): ParsedArgs {
-  const parsedArgs: ParsedArgs = new Map()
-  parsedArgs.set('_', [])
-
-  let currentOption: string | null = null
-
-  // Every argument that starts with a dash is an option.
-  // Can start with one or two dashes.
-  // Can have multiple times the same option.
-  for (let i = 0; i < args.length; i++) {
-    const arg = args[i]
-    if (arg.startsWith('--')) {
-      currentOption = arg.slice(2)
-      if (!parsedArgs.has(currentOption)) {
-        parsedArgs.set(currentOption, [])
-      }
-      if (booleans.includes(currentOption)) {
-        currentOption = null
-      }
-    } else if (arg.startsWith('-')) {
-      currentOption = arg.slice(1)
-      if (currentOption.length > 1) {
-        // Handle stacked short options
-        for (let j = 0; j < currentOption.length; j++) {
-          const option = currentOption.charAt(j)
-          if (!parsedArgs.has(option)) {
-            parsedArgs.set(option, [])
-          }
-        }
-        currentOption = null
-      } else if (!parsedArgs.has(currentOption)) {
-        parsedArgs.set(currentOption, [])
-      }
-      if (currentOption && booleans.includes(currentOption)) {
-        currentOption = null
-      }
-    } else if (currentOption) {
-      const option = parsedArgs.get(currentOption)
-      option.push(arg)
-      parsedArgs.set(currentOption, option)
-      currentOption = null
-    } else {
-      if (!parsedArgs.has('_')) {
-        parsedArgs.set('_', [])
-      }
-      const option = parsedArgs.get('_')
-      option.push(arg)
-      parsedArgs.set('_', option)
-    }
-  }
-
-  return parsedArgs
 }
--- tests/command.test.ts
+++ tests/command.test.ts
@@ -1,7 +1,7 @@
 /// <reference types="@as-pect/assembly/types/as-pect" />
-import { Command, parseArgs } from '../assembly/command'
+import { Command } from '../assembly/command'
 
-describe('Program', () => {
+describe('Command', () => {
   it('prints a simple help message', () => {
     const program = new Command('test')
 
@@ -30,91 +30,5 @@ describe('Program', () => {
     expect(program.help()).toBe(
       'Usage: test [options] [arguments]\n\nOptions:\n\t-h, --help\tPrints help message\n\t-p, --peppers\tAdd peppers\n\t-o, --onions\tAdd onions\n\t-b, --bbq-sauce\tAdd bbq sauce'
     )
-  })
-
-  it('parses options with arguments', () => {
-    const args = parseArgs(['-p', '-o', 'foo', 'bar'])
-
-    expect(args.has('p')).toBe(true)
-    expect(args.get('p').length).toBe(0)
-
-    expect(args.has('o')).toBe(true)
-    expect(args.get('o').length).toBe(1)
-    expect(args.get('o')).toInclude('foo')
-
-    expect(args.has('_')).toBe(true)
-    expect(args.get('_').length).toBe(1)
-    expect(args.get('_')).toInclude('bar')
-  })
-
-  it('parses long options with arguments', () => {
-    const args = parseArgs(['--peppers', '--onions', 'foo', 'bar'])
-
-    expect(args.has('peppers')).toBe(true)
-    expect(args.get('peppers').length).toBe(0)
-
-    expect(args.has('onions')).toBe(true)
-    expect(args.get('onions').length).toBe(1)
-    expect(args.get('onions')).toInclude('foo')
-
-    expect(args.has('_')).toBe(true)
-    expect(args.get('_').length).toBe(1)
-    expect(args.get('_')).toInclude('bar')
-  })
-
-  it('parses many arguments', () => {
-    const args = parseArgs(['foo', 'bar', 'baz'])
-
-    expect(args.has('_')).toBe(true)
-    expect(args.get('_').length).toBe(3)
-    expect(args.get('_')).toInclude('foo')
-    expect(args.get('_')).toInclude('bar')
-    expect(args.get('_')).toInclude('baz')
-  })
-
-  it('parses stacked short options', () => {
-    const args = parseArgs(['-pfo'])
-
-    expect(args.has('p')).toBe(true)
-    expect(args.get('p').length).toBe(0)
-
-    expect(args.has('f')).toBe(true)
-    expect(args.get('f').length).toBe(0)
-
-    expect(args.has('o')).toBe(true)
-    expect(args.get('o').length).toBe(0)
-  })
-
-  it('handles boolean short options', () => {
-    const args = parseArgs(['-p', '-o', 'foo', 'bar'], ['p', 'o'])
-
-    expect(args.has('p')).toBe(true)
-    expect(args.get('p').length).toBe(0)
-
-    expect(args.has('o')).toBe(true)
-    expect(args.get('o').length).toBe(0)
-
-    expect(args.has('_')).toBe(true)
-    expect(args.get('_').length).toBe(2)
-    expect(args.get('_')).toInclude('foo')
-    expect(args.get('_')).toInclude('bar')
-  })
-
-  it('handles boolean long options', () => {
-    const args = parseArgs(
-      ['--peppers', '--onions', 'foo', 'bar'],
-      ['peppers', 'onions']
-    )
-
-    expect(args.has('peppers')).toBe(true)
-    expect(args.get('peppers').length).toBe(0)
-
-    expect(args.has('onions')).toBe(true)
-    expect(args.get('onions').length).toBe(0)
-
-    expect(args.has('_')).toBe(true)
-    expect(args.get('_').length).toBe(2)
-    expect(args.get('_')).toInclude('foo')
-    expect(args.get('_')).toInclude('bar')
   })
 })
